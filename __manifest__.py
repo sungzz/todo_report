afe796f08a831b26824e36c29c26f8138efaf6f1
sung <khisungz@gmail.com>
2018-07-26 10:08:13 +0700

    update chapter 10 Qweb Report

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'To-do Report',
+    'description': 'Report for to-do tasks.',
+    'author': 'Santoni',
+    'depends': ['todo_kanban'],
+    'data':[
+        'reports/todo_report.xml',
+        'reports/todo_model_report.xml'
+    ],
+    'application': True
+
+}
